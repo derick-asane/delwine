23560869416d978ad2e4157dceaff2340521c094
derick-asane <derickasane@gmail.com>
2024-06-04 17:23:24 +0100

    add informations and the different links at the level of the footer

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,42 +1,65 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const MyLinks = [
+    {
+      id: 1,
+      link: "/",
+      name: "Home",
+    },
+    {
+      id: 1,
+      link: "products",
+      name: "Products",
+    },
+    {
+      id: 2,
+      link: "service",
+      name: "Service",
+    },
+    {
+      id: 3,
+      link: "contact",
+      name: "Contact-Us",
+    },
+    {
+      id: 4,
+      link: "about-us",
+      name: "About-Us",
+    },
+  ];
   return (
     <footer className="bg-gray-800 text-white py-8 w-full">
       <div className="container mx-auto flex flex-wrap justify-between">
         <div className="w-full sm:w-1/2 md:w-auto mb-4 sm:mb-0">
           <h2 className="text-xl font-bold mb-2">About Us</h2>
           <p className="text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec
-            turpis nec urna luctus consectetur.
+            Our store is located at Yaounde VI , carrefour Simbock, opposite
+            phamarcy simbock
           </p>
         </div>
         <div className="w-full sm:w-1/2 md:w-auto mb-4 sm:mb-0">
           <h2 className="text-xl font-bold mb-2">Links</h2>
           <ul className="text-sm">
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-gray-300 hover:text-white">
-                Contact
-              </a>
-            </li>
+            {MyLinks.map((mylink) => (
+              <li key={mylink.id}>
+                <Link
+                  to={mylink.link}
+                  className="text-gray-300 hover:text-white"
+                >
+                  {mylink.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="w-full sm:w-1/2 md:w-auto">
           <h2 className="text-xl font-bold mb-2">Contact Us</h2>
           <p className="text-sm">
-            Email: example@example.com
+            Email: derickasane@gmail.com
             <br />
-            Phone: 123-456-7890
+            Phone: 674-018-433
           </p>
         </div>
       </div>
